9cdb10f0963066fa190533c62f65e40b45b2e467
Chris Ford <christophertford@gmail.com>
2016-04-22 18:30:12 +0100

    Pull out query params.

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function (request, response) {
+  response.send('Hello World!' + request.query.foo);
 });
 
 app.listen(3000, function () {
