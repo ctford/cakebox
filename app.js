efaa09e34f579b47d7af0279ef2ab49cd311a988
Chris Ford <christophertford@gmail.com>
2016-04-22 21:58:42 +0100

    Render a React component as an HTML fragment.

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+
+var CommentBox = React.createClass({displayName: 'CommentBox',
+  render: function() {
+    return (
+      React.createElement('div', {className: "commentBox"},
+        "Hello, world! I am a CommentBox."
+      )
+    );
+  }
+});
+
+var CreateCommentBox = React.createFactory(CommentBox);
 
 var express = require('express');
 var app = express();
 
 app.get('/:module/:component', function (request, response) {
-  response.send('Module: ' + request.params.module + 'Component: ' + request.params.component + request.query.foo);
+  response.send(ReactDOMServer.renderToString(CreateCommentBox()));
 });
 
 app.listen(3000, function () {
