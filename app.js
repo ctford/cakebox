05fe1336d058d83157eb638dc81475e8e65800e6
Chris Ford <christophertford@gmail.com>
2016-04-22 22:13:31 +0100

    Read props from the query string.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var CommentBox = React.createClass({displayName: 'CommentBox',
   render: function() {
     return (
       React.createElement('div', {className: "commentBox"},
-        "Hello, world! I am a CommentBox."
+        "Hello, world! I am a " + this.props.name
       )
     );
   }
@@ -17,11 +17,10 @@ var express = require('express');
 var app = express();
 
 app.get('/:module/:component', function (request, response) {
-  var component = CreateCommentBox();
+  var component = CreateCommentBox(request.query);
   response.send(ReactDOMServer.renderToStaticMarkup(component));
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
