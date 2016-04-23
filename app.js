4efc7b30c2b7d5bd200cd94c93ef5a34d521ac04
Chris Ford <christophertford@gmail.com>
2016-04-23 18:15:13 +0100

    Render a real component from NPM.

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
 var React = require('react');
 var ReactDOMServer = require('react-dom/server');
-
-var comment = {commentBox:
-    React.createClass({displayName: 'CommentBox',
-      render: function() {
-        return (
-          React.createElement('div', {className: "commentBox"},
-            "Hello, world! I am a " + this.props.name
-          )
-        );
-      }
-})};
-
-var modules = {comment: comment};
 var express = require('express');
 var app = express();
 
-// List the components available for the module.
-app.get('/:module', function (request, response) {
-  var module = modules[request.params.module];
-  response.send(Object.keys(module));
-});
+var modules = {timepicker: require('rc-time-picker')};
 
-// Render the specified component.
-app.get('/:module/:component', function (request, response) {
-  var Factory = React.createFactory(modules[request.params.module][request.params.component]);
+app.get('/:module', function (request, response) {
+  var Factory = React.createFactory(modules[request.params.module]);
   var component = Factory(request.query);
-  response.send(ReactDOMServer.renderToStaticMarkup(component));
+  var fragment = ReactDOMServer.renderToStaticMarkup(component);
+  response
+    .type("text/plain")
+    .send(fragment);
 });
 
 app.listen(3000, function () {
