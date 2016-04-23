e1a78740e62131f4fba882e54ef9a900ce0e4adb
Chris Ford <christophertford@gmail.com>
2016-04-23 18:30:11 +0100

    Add a search as well.

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var ReactDOMServer = require('react-dom/server');
 var express = require('express');
 var app = express();
 
-var modules = {timepicker: require('rc-time-picker')};
+var modules = {timepicker: require('rc-time-picker'),
+               search: require('apeman-react-search').ApSearch};
 
 app.get('/:module', function (request, response) {
   var Factory = React.createFactory(modules[request.params.module]);
